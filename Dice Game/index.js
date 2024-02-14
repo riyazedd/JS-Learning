f057b6e46cc1a02ad1e2de2a16d47fa207a6c6e0
riyazedd <riyaz.shrestha00@gmail.com>
2024-02-14 19:13:59 +0545

    function added

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,34 +1,34 @@
 function rollDice(){
 
-//FOR DICE 1
+    //FOR DICE 1
 
-let randomNumber1= Math.floor(Math.random()*6)+1; //generats random number from 1-6
+    let randomNumber1= Math.floor(Math.random()*6)+1; //generats random number from 1-6
 
-let randomDiceImage1=`dice${randomNumber1}.png`; //set name of image according to random number dice1.png-dice6.png
+    let randomDiceImage1=`dice${randomNumber1}.png`; //set name of image according to random number dice1.png-dice6.png
 
-let randomImageSource1=`images/${randomDiceImage1}`;  //sets source of image images/dice1.png-images/dice6.png
+    let randomImageSource1=`images/${randomDiceImage1}`;  //sets source of image images/dice1.png-images/dice6.png
 
-document.querySelectorAll('img')[0].setAttribute('src',randomImageSource1);  //changes image source of first dice
+    document.querySelectorAll('img')[0].setAttribute('src',randomImageSource1);  //changes image source of first dice
 
-//FOR DICE 2
+    //FOR DICE 2
 
-let randomNumber2= Math.floor(Math.random()*6)+1; //generats random number from 1-6
+    let randomNumber2= Math.floor(Math.random()*6)+1; //generats random number from 1-6
 
-let randomDiceImage2=`dice${randomNumber2}.png`; //set name of image according to random number dice1.png-dice6.png
+    let randomDiceImage2=`dice${randomNumber2}.png`; //set name of image according to random number dice1.png-dice6.png
 
-let randomImageSource2=`images/${randomDiceImage2}`;  //sets source of image images/dice1.png-images/dice6.png
+    let randomImageSource2=`images/${randomDiceImage2}`;  //sets source of image images/dice1.png-images/dice6.png
 
-document.querySelectorAll('img')[1].setAttribute('src',randomImageSource2);  //changes image source of second dice
+    document.querySelectorAll('img')[1].setAttribute('src',randomImageSource2);  //changes image source of second dice
 
 
-//DECLARATION OF WINNER
+    //DECLARATION OF WINNER
 
-if(randomNumber1>randomNumber2){
-    document.querySelector('h1').innerHTML='🚩Player 1 Wins!!';
-}else if(randomNumber1<randomNumber2){
-    document.querySelector('h1').innerHTML='Player 2 Wins!!🚩';
-}else{
-    document.querySelector('h1').innerHTML='🚩Draw!!🚩';
-}
+    if(randomNumber1>randomNumber2){
+        document.querySelector('h1').innerHTML='🚩Player 1 Wins!!';
+    }else if(randomNumber1<randomNumber2){
+        document.querySelector('h1').innerHTML='Player 2 Wins!!🚩';
+    }else{
+        document.querySelector('h1').innerHTML='🚩Draw!!🚩';
+    }
 
 }
