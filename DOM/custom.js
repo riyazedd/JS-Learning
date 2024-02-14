c3ef0e42024d7a37f7687f9b27ea71b2cffa06cf
riyazedd <riyaz.shrestha00@gmail.com>
2024-02-14 18:54:30 +0545

    DOM

---
+++ DOM/custom.js
@@ -0,0 +1,4 @@
+// document.querySelector('h1').classList.add('huge');
+
+// document.querySelector('a').getAttribute('href');
+// document.querySelector('a').setAttribute('href','https://youtube.com');
