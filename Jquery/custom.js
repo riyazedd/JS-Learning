e9af2507d9e032ea8c3ce5d3328dfeaab8265ddf
riyazedd <riyaz.shrestha00@gmail.com>
2024-02-16 21:36:27 +0545

    Simon Game

---
+++ Jquery/custom.js
@@ -0,0 +1,15 @@
+
+// $('h1').css('color','green'); //$ can be written as jQuery
+
+// // $('h1').addClass('big-text'); 
+
+// $('button').text('i am a button')
+
+// $('a').attr('href','https://google.com')
+
+// //Event Listerners
+// $('input').keypress(function(event){
+//     $('h1').text(event.key);
+// })
+
+$('h1').append(" World")
