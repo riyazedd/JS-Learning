99982e24a09a4eb4191a8625a054fd9b5e3d6716
riyazedd <riyaz.shrestha00@gmail.com>
2024-02-17 16:22:51 +0545

    drum kit with use of jquery

---
+++ Drum Kit Using JQuery/index.js
@@ -0,0 +1,62 @@
+//detecting button press
+$('button').click(function(){
+    var button=$(this).text();
+    console.log(button);
+    makeSound(button);
+    animatePress(button);
+})
+
+//detecting keypress
+$(document).keypress(function(event){
+    makeSound(event.key);
+    animatePress(event.key);
+})
+
+//playing sound
+function makeSound(key){
+    switch(key){
+        case 'w':
+            let tom1=new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        
+        case 'a':
+            let tom2=new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        
+        case 's':
+            let tom3=new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        
+        case 'd':
+            let tom4=new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        
+        case 'j':
+            let kick=new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        
+        case 'k':
+            let crash=new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        
+        case 'l':
+            let snare=new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+    }
+
+}
+
+//animation on press
+function animatePress(key){
+    $('.'+key).addClass('pressed');
+    setTimeout(function(){
+        $('.'+key).removeClass('pressed'); 
+    },200);
+}
