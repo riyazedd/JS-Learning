33ac95474c1113fd41dd1f9d8d92993aaea990a9
riyazedd <riyaz.shrestha00@gmail.com>
2024-02-18 17:38:50 +0545

    Sound Added

--- Pomodoro Timer/custom.js
+++ Pomodoro Timer/custom.js
@@ -2,6 +2,7 @@ let timeInterval;
 let minutes = 25;
 let seconds = 0;
 let isBreak = false;
+let audio=new Audio('sounds/alarm.wav'); //audio object to play when work time or break time is finished
 
 const timerDisplay = $('.display');
 const startButton = $('#startTimer');
@@ -17,13 +18,15 @@ function startTimer() {
     }
     if (!isBreak) {
         // If it's not a break time, start work timer
-        timeInterval = setInterval(updateTimer, 1000);
+        timeInterval = setInterval(updateTimer, 10);
     } else {
         // If it's break time, start break timer
-        timeInterval = setInterval(breakTimer, 1000);
+        timeInterval = setInterval(breakTimer, 10);
     }
+    audio.pause();
 }
 
+//starting work time
 function updateTimer() {
     if (seconds === 0) {
         if (minutes === 0) {
@@ -34,6 +37,7 @@ function updateTimer() {
             seconds = 0;
             isBreak = true;
             startButton.prop('disabled', false); // Enable start button for the break
+            audio.play();
             return;
         }
         minutes--;
@@ -44,6 +48,7 @@ function updateTimer() {
     timerDisplay.text(`${padTime(minutes)}:${padTime(seconds)}`);
 }
 
+//starting break time
 function breakTimer() {
     if (seconds === 0) {
         if (minutes === 0) {
@@ -51,6 +56,7 @@ function breakTimer() {
             timerDisplay.text('00:00');
             isBreak = false;
             startButton.prop('disabled', false); // Enable start button for the next work session
+            audio.play();
             return;
         }
         minutes--;
@@ -61,6 +67,8 @@ function breakTimer() {
     timerDisplay.text(`${padTime(minutes)}:${padTime(seconds)}`);
 }
 
+
+//resetting the timer
 function resetTimer() {
     $('h2').text('Get Working');
     clearInterval(timeInterval);
@@ -69,8 +77,11 @@ function resetTimer() {
     isBreak = false;
     startButton.prop('disabled', false);
     timerDisplay.text(`${padTime(minutes)}:${padTime(seconds)}`);
+    audio.pause();
 }
 
+
+//padding time (if 1 the show 01)
 function padTime(time) {
     return time.toString().padStart(2, '0');
 }
